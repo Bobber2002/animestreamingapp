1bb689fb6f05e216983e883a04219410d6518191
ChristofferRTS <4906240@RTS-365.dk>
2022-10-27 21:32:56 +0200

    update router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import { useState } from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AnimePage from "./Pages/Anime/page";
 import SearchPage from "./Pages/Search/page";
 
 function App() {
   return (
     <div className="App w-screen h-screen overflow-x-hidden">
-      <Router>
         <Routes>
           <Route path="/" element={<SearchPage/>} />
           <Route path="/anime" element={<AnimePage/>} />
           <Route path="/test" element={<p>Hello</p>} />
         </Routes>
-      </Router>
     </div>
   );
 }
