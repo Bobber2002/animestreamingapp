3d93f31d71672475253b53aefb52c37ca4984700
Daniel <danielverdugomathiesen@gmail.com>
2022-10-29 22:03:31 +0200

    feat: :lipstick: Add high-contrast ui style
    Add high-contrast ui style in the shades of green, and search improvements.

--- src/Pages/Search/page.js
+++ src/Pages/Search/page.js
@@ -5,19 +5,32 @@ import { Link } from "react-router-dom";
 
 const SearchPage = ({ setCurrAnime }) => {
   const [data, setData] = useState([]);
+  const [searching, setSearching] = useState(false);
 
   function handleSearch(e) {
     e.preventDefault();
-    axios
-      .get(
-        "https://calm-tan-reindeer-slip.cyclic.app/gogoanime/search?keyw=" +
-          e.currentTarget.children[0].value +
-          "&page=0"
-      )
-      .then((res) => {
-        console.log(res);
-        setData(res.data);
-      });
+    console.log(e.currentTarget.children[0].value);
+    if (e.currentTarget.children[0].value != "") {
+      setSearching(true);
+      handleScrollTo();
+      axios
+        .get(
+          "https://calm-tan-reindeer-slip.cyclic.app/gogoanime/search?keyw=" +
+            e.currentTarget.children[0].value +
+            "&page=0"
+        )
+        .then((res) => {
+          console.log(res);
+          setData(res.data);
+        });
+    }
+  }
+
+  function handleScrollTo() {
+    document.getElementById("videos_container").style.display = "grid";
+    document
+      .getElementById("videos_container")
+      .scrollIntoView({ behavior: "smooth" });
   }
 
   function handleStopScroll(e) {
@@ -40,28 +53,40 @@ const SearchPage = ({ setCurrAnime }) => {
     }
   }
   return (
-    <>
-      <div id="form_container">
-        <form action="" onSubmit={handleSearch}>
+    <main className="bg-animeBlack">
+      <div
+        id="form_container"
+        className="w-full h-screen flex justify-evenly items-center flex-col"
+      >
+        <div>
+          <h1 className="text-6xl font-bold text-animeDarkGreen text-center uppercase">
+            Little peter spider
+          </h1>
+          <br />
+          <p className="text-4xl font-bold text-animeDarkGreen text-center uppercase">
+            Anime streaming
+          </p>
+        </div>
+        <form action="" onSubmit={handleSearch} className="w-full px-[30%]">
           <input
             type="text"
             name=""
             id=""
             placeholder="Search for an anime"
-            className="bg-gray-200 outline-none border-none p-4"
+            className="bg-transparent border-2 border-animeLightGreen p-4 w-full focus:outline-0 text-animeDarkGreen placeholder:text-animeDarkGreen"
           />
         </form>
       </div>
       <div
         id="videos_container"
-        className="h-full w-full p-6 grid grid-cols-4 gap-y-4 justify-items-center"
+        className="h-screen w-full p-6 hidden grid-cols-4 gap-y-10 justify-items-center"
       >
         {data.map((anime) => {
           return (
-            <Link to={"/anime?id="+anime.animeId}>
+            <Link to={"/anime?id=" + anime.animeId}>
               <div
                 id={anime.animeId}
-                className="w-[20vw] h-[25vw] bg-slate-200 flex items-center flex-col gap-3"
+                className="w-[20vw] h-[25vw] bg-transparent border-2 border-animeLightGreen flex items-center flex-col gap-3"
                 onMouseEnter={handleStopScroll}
                 onMouseLeave={handleStartScroll}
               >
@@ -73,11 +98,11 @@ const SearchPage = ({ setCurrAnime }) => {
                       backgroundImage: `url(${anime.animeImg})`,
                     }}
                   />
-                  <div className="w-full px-4">
+                  <div className="w-full px-2 pt-5 text-center text-animeDarkGreen">
                     <Marquee gradient={false} speed={20} className="w-full">
                       {anime.animeTitle + "⠀⠀⠀"}
                     </Marquee>
-                    <p>{anime.status}</p>
+                    <p className="pt-4">{anime.status}</p>
                   </div>
                 </div>
               </div>
@@ -85,7 +110,7 @@ const SearchPage = ({ setCurrAnime }) => {
           );
         })}
       </div>
-    </>
+    </main>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        animeBlack: "#000501",
+        animeDarkGreen: "#73AB84",
+        animeLightGreen: "#99D19C",
+        animeDarkTeal: "#79C7C5",
+        animeTeal: "#ADE1E5",
+      }
+    },
   },
   plugins: [],
 };
